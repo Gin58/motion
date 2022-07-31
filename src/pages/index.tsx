92ad6e9062815f799cfb8c853a1e70e4e1ca507c
Gin58 <invincible.gnj1875@gmail.com>
2022-07-31 15:51:30 +0900

    add framer

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,17 @@
 import type { NextPage } from 'next'
+import { motion } from 'framer-motion'
 
 const Home: NextPage = () => {
-  return <div>hello</div>
+  return (
+    <div className="flex justify-center items-center w-screen h-screen">
+      <motion.button
+        whileTap={{ scale: 1.5 }}
+        className="rounded-lg w-[100px] h-[30px] bg-white text-black"
+      >
+        ★
+      </motion.button>
+    </div>
+  )
 }
 
 export default Home
